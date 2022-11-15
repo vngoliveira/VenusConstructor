d7b395a1c19c53ea0ece13d2c1ed8cd7bd183640
vngoliveira <vivi40195@gmail.com>
2022-11-15 20:26:28 -0300

    start spring boot project

---
+++ api-construcao/src/main/java/com/venusconstructor/apiconstrucao/VenusConstructorApplication.java
@@ -0,0 +1,13 @@
+package com.venusconstructor.apiconstrucao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VenusConstructorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VenusConstructorApplication.class, args);
+	}
+
+}
---
+++ api-construcao/src/test/java/com/venusconstructor/apiconstrucao/VenusConstructorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.venusconstructor.apiconstrucao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VenusConstructorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
